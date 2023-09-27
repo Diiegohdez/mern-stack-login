f432c5514b5701d530a9d3742fd4a3122e495db5
Diiegohdez <diiego.hdez@gmail.com>
2023-09-27 17:43:12 -0500

    solucion

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import app from "./src/app.js";
-import {connectBD} from "./src/db.js";
+import { connectBD } from "./src/db.js";
+import { PORT } from "./src/confing.js";
 
 
 connectBD();
-app.listen(4000)
-console.log('serve esta activo', 4000)
+app.listen(PORT)
+console.log('serve esta activo', PORT)
