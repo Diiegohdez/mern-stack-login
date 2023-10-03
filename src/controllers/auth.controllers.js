535122809f2540a1171a8fd97067a5b0a39f159b
Diiegohdez <diiego.hdez@gmail.com>
2023-10-03 17:12:19 -0500

    modific cookies

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -52,7 +52,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: process.env.NODE_ENV !== "development",
             secure: true,
-            sameSite: "none",
+            sameSite: 'lax'
         });
         res.json({
             id: userFound._id,
